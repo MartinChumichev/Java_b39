0ecf064a18e4e26b060dff273a40bc65642057e6
Martin <Kynep911@yandex.ru>
2023-06-06 21:31:54 +0300

    Добавлены тесты расстояния

--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
@@ -5,9 +5,21 @@ import org.testng.annotations.Test;
 
 @Test
 public class DistanceTests {
+    // Релевантные значения координат
     public void testPoint() {
-        Point p = new Point(1, 1, 2, 2);
-        Assert.assertEquals(p.distance(), 0);
+        Point p = new Point(2, 3, 4, 5);
+        Assert.assertEquals(p.distance(), 1.4142135623730951);
+    }
 
+    // Одинаковые координаты точек
+    public void testIdentical() {
+        Point p = new Point(1, 1, 1, 1);
+        Assert.assertEquals(p.distance(), 0);
+    }
+
+    // Дробные координаты
+    public void testFraction() {
+        Point p = new Point(1.2,7,7.9,11.2);
+        Assert.assertEquals(p.distance(), 6.673080248281148);
     }
 }
