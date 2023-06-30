30b7d14e11e30680e96abebd1291b817e16d1c7b
Martin <Kynep911@yandex.ru>
2023-06-30 17:12:16 +0300

    Запуск тестов в 3х разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,17 +2,34 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.Browser;
 
 import java.time.Duration;
 
 public class ApplicationManager {
+
+
     WebDriver wd;
     private ContactHelper contactHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    public String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new ChromeDriver();
+        if (browser.equals(Browser.CHROME.browserName())) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(Browser.FIREFOX.browserName())) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(Browser.IE.browserName())) {
+            wd = new InternetExplorerDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
