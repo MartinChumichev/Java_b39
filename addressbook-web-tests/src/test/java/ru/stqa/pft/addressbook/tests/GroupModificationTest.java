ea2a36344286bce95c667a9a32d2b3c7caaddce7
Martin <Kynep911@yandex.ru>
2023-07-14 10:33:20 +0300

    Сравнение количества групп до и после проведения тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,6 +9,7 @@ public class GroupModificationTest extends TestBase {
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("Test2", null, null));
         }
@@ -15,7 +17,9 @@ public class GroupModificationTest extends TestBase {
         app.getGroupHelper().editGroupForm();
         app.getGroupHelper().fillGroupForm(new GroupData("123", "ggh", "gf41"));
         app.getGroupHelper().updateGroupForm();
-        app.getGroupHelper().returnToGroupPage();
+               app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before);
     }
 
 
